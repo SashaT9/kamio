706a8257b27c618bf776276a9d25c1d57596bb38
Oleksandr Tymkovych <oleksandr.tymkovych@student.uj.edu.pl>
2025-08-20 22:02:50 +0200

    C11 submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
+#include "CLI/CLI.hpp"
 #include <iostream>
 
-int main() {
-	std::cout << "Hello, World!\n";
+int main(int argc, char **argv) {
+	CLI::App app{"App description"};
+
+	// Define options
+	int p = 0;
+	app.add_option("-p", p, "Parameter");
+
+	CLI11_PARSE(app, argc, argv);
+
+	std::cout << "Parameter value: " << p << std::endl;
+	return 0;
 }
